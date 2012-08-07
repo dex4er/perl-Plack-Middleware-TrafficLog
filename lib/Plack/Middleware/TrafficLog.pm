c6674282e42ffbf279f2e7404396240b9f89d427
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2012-08-07 17:31:10 +0200

    POD tells about delayed response

--- lib/Plack/Middleware/TrafficLog.pm
+++ lib/Plack/Middleware/TrafficLog.pm
@@ -18,6 +18,10 @@ Plack::Middleware::TrafficLog - Log headers and body of HTTP traffic
 This middleware logs the request and response messages with detailed
 information.
 
+This module works also with applications which have delayed response. In that
+case each chunk is logged separately and shares the same ID number and
+headers.
+
 =for readme stop
 
 =cut
