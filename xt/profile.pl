693bdd391b735a3235de83986c9dc8d8e29ecad9
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2012-08-07 13:12:27 +0200

    Test script for profiler

--- xt/profile.pl
+++ xt/profile.pl
@@ -2,8 +2,27 @@
 
 use lib 'lib', '../lib';
 
-use DEXTER::Module::Skeleton;
+use HTTP::Request::Common;
+use Plack::Test;
+use Plack::Builder;
 
-DEXTER::Module::Skeleton->hello;
+my $test = sub {
+    my @args = @_;
+    return sub {
+        my ($req) = @_;
+        my $app = builder {
+            enable 'Plack::Middleware::TrafficLog', logger => sub { }, @args;
+            sub { [ 200, [ 'Content-Type' => 'text/plain' ], [ "OK\nOK\n" ] ] };
+        };
+        test_psgi $app, sub { $_[0]->($req) };
+    };
+};
+
+my $req = POST 'http://example.com/', Content => "TEST\nTEST\n";
+$req->header('Host' => 'example.com', 'Content-Type' => 'text/plain');
+
+foreach (1..10000) {
+    $test->()->($req);
+};
 
 print "nytprof.out data collected. Call nytprofhtml --open\n";
