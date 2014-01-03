498956fd8ae47a797e8e0b6acd2fbc408381b797
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2014-01-04 00:17:04 +0100

    Bump year

--- lib/Plack/Middleware/TrafficLog.pm
+++ lib/Plack/Middleware/TrafficLog.pm
@@ -261,7 +261,7 @@ Piotr Roszatycki <dexter@cpan.org>
 
 =head1 LICENSE
 
-Copyright (c) 2012 Piotr Roszatycki <dexter@cpan.org>.
+Copyright (c) 2012, 2014 Piotr Roszatycki <dexter@cpan.org>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as perl itself.
