5ebedf0217ff83c2f470e1481f4b3055734aea02
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2015-10-14 17:10:00 +0200

    Bump version and year

--- README.md
+++ README.md
@@ -108,7 +108,7 @@ Piotr Roszatycki <dexter@cpan.org>
 
 # LICENSE
 
-Copyright (c) 2012, 2014 Piotr Roszatycki <dexter@cpan.org>.
+Copyright (c) 2012, 2014-2015 Piotr Roszatycki <dexter@cpan.org>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as perl itself.
--- lib/Plack/Middleware/TrafficLog.pm
+++ lib/Plack/Middleware/TrafficLog.pm
@@ -43,7 +43,7 @@ use 5.008;
 use strict;
 use warnings;
 
-our $VERSION = '0.0400';
+our $VERSION = '0.0401';
 
 
 use parent 'Plack::Middleware';
@@ -269,7 +269,7 @@ Piotr Roszatycki <dexter@cpan.org>
 
 =head1 LICENSE
 
-Copyright (c) 2012, 2014 Piotr Roszatycki <dexter@cpan.org>.
+Copyright (c) 2012, 2014-2015 Piotr Roszatycki <dexter@cpan.org>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as perl itself.
